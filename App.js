2a0220fcd1739681ef0c114ad03b5b83ad42b6c9
alierensevinc <sevinc.aeren@gmail.com>
2025-11-16 01:32:37 +0300

    Init Project

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaProvider} from "react-native-safe-area-context";
+
+function HomeScreen() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Home Screen</Text>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen}/>
+        </Stack.Navigator>
+    );
+}
+
+export default function App() {
+    return (
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <RootStack/>
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
+}
