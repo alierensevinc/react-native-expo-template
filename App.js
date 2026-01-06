7a1f6fb041ab0ef62fff95514c504702155d81c3
alierensevinc <sevinc.aeren@gmail.com>
2026-01-06 12:21:02 +0300

    feat: Add Montserrat font family, dynamic status bar styling, and dark mode splash screen support.

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+// eslint-disable-next-line import/namespace
 import { StatusBar } from 'expo-status-bar';
 import { useCallback, useEffect, useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -10,7 +12,7 @@ import Toast from 'react-native-toast-message';
 
 import './src/i18n';
 import { QueryProvider } from './src/context/QueryProvider';
-import { ThemeProvider } from './src/context/ThemeContext';
+import { ThemeProvider, useTheme } from './src/context/ThemeContext';
 import ComponentsScreen from './src/screens/ComponentsScreen';
 import DetailScreen from './src/screens/DetailScreen';
 import HomeScreen from './src/screens/HomeScreen';
@@ -18,6 +20,11 @@ import HomeScreen from './src/screens/HomeScreen';
 SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
+
+function ThemedStatusBar() {
+  const { isDark } = useTheme();
+  return <StatusBar style={isDark ? 'light' : 'dark'} />;
+}
 
 function RootStack() {
   return (
@@ -35,6 +42,26 @@ export default function App() {
   useEffect(() => {
     async function prepare() {
       try {
+        await Font.loadAsync({
+          'Montserrat-Black': require('./assets/fonts/montserrat/Montserrat-Black.ttf'),
+          'Montserrat-BlackItalic': require('./assets/fonts/montserrat/Montserrat-BlackItalic.ttf'),
+          'Montserrat-Bold': require('./assets/fonts/montserrat/Montserrat-Bold.ttf'),
+          'Montserrat-BoldItalic': require('./assets/fonts/montserrat/Montserrat-BoldItalic.ttf'),
+          'Montserrat-ExtraBold': require('./assets/fonts/montserrat/Montserrat-ExtraBold.ttf'),
+          'Montserrat-ExtraBoldItalic': require('./assets/fonts/montserrat/Montserrat-ExtraBoldItalic.ttf'),
+          'Montserrat-ExtraLight': require('./assets/fonts/montserrat/Montserrat-ExtraLight.ttf'),
+          'Montserrat-ExtraLightItalic': require('./assets/fonts/montserrat/Montserrat-ExtraLightItalic.ttf'),
+          'Montserrat-Italic': require('./assets/fonts/montserrat/Montserrat-Italic.ttf'),
+          'Montserrat-Light': require('./assets/fonts/montserrat/Montserrat-Light.ttf'),
+          'Montserrat-LightItalic': require('./assets/fonts/montserrat/Montserrat-LightItalic.ttf'),
+          'Montserrat-Medium': require('./assets/fonts/montserrat/Montserrat-Medium.ttf'),
+          'Montserrat-MediumItalic': require('./assets/fonts/montserrat/Montserrat-MediumItalic.ttf'),
+          'Montserrat-Regular': require('./assets/fonts/montserrat/Montserrat-Regular.ttf'),
+          'Montserrat-SemiBold': require('./assets/fonts/montserrat/Montserrat-SemiBold.ttf'),
+          'Montserrat-SemiBoldItalic': require('./assets/fonts/montserrat/Montserrat-SemiBoldItalic.ttf'),
+          'Montserrat-Thin': require('./assets/fonts/montserrat/Montserrat-Thin.ttf'),
+          'Montserrat-ThinItalic': require('./assets/fonts/montserrat/Montserrat-ThinItalic.ttf'),
+        });
         await new Promise((resolve) => setTimeout(resolve, 1000));
       } catch (e) {
         console.warn(e);
@@ -62,7 +89,7 @@ export default function App() {
         <SafeAreaProvider>
           <QueryProvider>
             <BottomSheetModalProvider>
-              <StatusBar style="auto" />
+              <ThemedStatusBar />
               <NavigationContainer>
                 <RootStack />
               </NavigationContainer>
